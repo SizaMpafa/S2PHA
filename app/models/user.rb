36f2403bdadcf34d354c99f18b8d716cc80de555
SizaMpafa <sizampafa972@gmail.com>
2025-03-23 16:01:28 +0200

    added a User model also included validations

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { customer: 0, owner: 1 }, _default: :customer
+
+  validates :first_name, :last_name, presence: true
+  validate :only_one_owner
+  before_destroy :prevent_owner_deletion
+
+  private
+
+  def only_one_owner
+    if role == "owner" && User.where(role: :owner).where.not(id: id).exists?
+      errors.add(:role, "only one owner is allowed")
+    end
+  end
+
+  def prevent_owner_deletion
+    throw(:abort) if owner?
+  end
+end
